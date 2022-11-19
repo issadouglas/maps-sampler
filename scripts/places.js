bdaf21b2354a0671338b9ba9a5102535772cb5aa
issadouglas <113636973+issadouglas@users.noreply.github.com>
2022-11-18 20:20:27 -0500

    Initial commit

---
+++ scripts/places.js
@@ -0,0 +1,22 @@
+places = 
+
+[{"placeName":"Riviera Beach","descrip":"1st most dangerous city in Florida","latitude":26.8201299567897,"longitude":-80.0552536498489},
+{"placeName":"West Palm Beach","descrip":"2nd most dangerous city in Florida","latitude":26.9684434443409,"longitude":-80.0929910124746},
+{"placeName":"Lauderhill","descrip":"3rd most dangerous city in Florida","latitude":26.558900892125,"longitude":-80.1884694085856},
+{"placeName":"Daytona Beach","descrip":"4th most dangerous city in Florida","latitude":29.5784951228312,"longitude":-81.0114990402353},
+{"placeName":"Miami Gardens","descrip":"5th most dangerous city in Florida","latitude":26.0224625032806,"longitude":-80.2112468375354},
+{"placeName":"Lake Worth","descrip":"6th most dangerous city in Florida","latitude":26.6458958692033,"longitude":-80.0655754792915},
+{"placeName":"Parkland","descrip":"7th most dangerous city in Florida","latitude":26.3618849770397,"longitude":-80.2466607482426},
+{"placeName":"Fort Pierce","descrip":"8th most dangerous city in Florida","latitude":27.8538990414964,"longitude":-80.2557965160459},
+{"placeName":"Fort Myers","descrip":"9th most dangerous city in Florida","latitude":26.7455652120160,"longitude":-81.8954777496492},
+{"placeName":"Sanford","descrip":"10th most dangerous city in Florida","latitude":29.1520887409285,"longitude":-81.2211343266962},
+{"placeName":"Orlando","descrip":"11th most dangerous city in Florida","latitude":28.6858917565001,"longitude":-81.3523117744001},
+{"placeName":"Jacksonville","descrip":"12th most dangerous city in Florida","latitude":30.7341157817250,"longitude":-81.6749993211950},
+{"placeName":"Panama City","descrip":"13th most dangerous city in Florida","latitude":30.3421733062841,"longitude":-85.6652373160036},
+{"placeName":"Ocala","descrip":"14th most dangerous city in Florida","latitude":29.4450445676793,"longitude":-82.0929581679040},
+{"placeName":"Tallahassee","descrip":"15th most dangerous city in Florida","latitude":30.9663924022231,"longitude":-84.1710252154158},
+{"placeName":"Miami","descrip":"16th most dangerous city in Florida","latitude":25.8101603197618,"longitude":-80.1614890149622},
+{"placeName":"Miami Beach","descrip":"17th most dangerous city in Florida","latitude":26.2147242314173,"longitude":-80.0601494122766},
+{"placeName":"Fort Lauderdale","descrip":"18th most dangerous city in Florida","latitude":26.1773979767802,"longitude":-80.1756077896044},
+{"placeName":"Pompano Beach","descrip":"19th most dangerous city in Florida","latitude":27.1307740424142,"longitude":-79.9489845279526},
+{"placeName":"St. Petersburg","descrip":"20th most dangerous city in Florida","latitude":27.8381374953040,"longitude":-82.5988588936511}]
